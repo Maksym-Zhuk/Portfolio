1e291fa0488e43e93be466389f65e75bd665b40f
Maksym-Zhuk <maksymzhuk759@gmail.com>
2025-07-27 01:10:02 +0300

    Add my photo to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from '@/components/Home/AboutMe';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -6,7 +7,14 @@ export default function Home() {
       <main className="w-full min-h-[90dvh] flex lg:flex-row flex-col justify-between">
         <AboutMe />
         <div className="lg:w-2/5 w-full lg:h-[90dvh] sm:h-[60dvh] h-[55dvh] flex lg:justify-end justify-center items-center">
-          <div className="lg:min-w-[440px] sm:w-[360px] w-[310px] lg:h-[580px] sm:h-[480px] h-[400px] bg-[#262626] rounded-4xl"></div>
+          <div className="lg:min-w-[440px] sm:w-[360px] w-[310px] lg:h-[580px] sm:h-[480px] h-[400px] bg-[#262626] rounded-4xl relative">
+            <Image
+              src={'/9c30ffef-d5e9-4e2a-b0e0-feccd2dd83bb.png'}
+              alt="Photo"
+              fill
+              className="rounded-4xl"
+            />
+          </div>
         </div>
       </main>
     </div>
